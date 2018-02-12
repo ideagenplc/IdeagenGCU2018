7f737c5da868c14481293fcf11f778a8469301cf
Ross Anderson <ross.anderson@ideagenplc.com>
2018-02-12 09:48:57 +0000

    updated publish

--- publish.ps1
+++ publish.ps1
@@ -3,9 +3,9 @@ dotnet publish .\TimelineLite\  -c Release  -f netcoreapp2.0 /p:GenerateRunTimeC
 Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.zip -Force
 Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.Timeline\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.Timeline.zip -Force
 Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.TimelineEvent\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.TimeLineEvent.zip -Force
-Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.TimelineEventAttachment\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.TimeLineEventAttachment.zip -Force
+Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.TimelineEventAttchment\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.TimeLineEventAttachment.zip -Force
 
 aws s3 cp .\TimelineLite.zip s3://stewartw-test-bucket/TimelineLite.zip
 aws s3 cp .\TimelineLite.Timeline.zip s3://stewartw-test-bucket/TimelineLite.Timeline.zip
 aws s3 cp .\TimelineLite.TimelineEvent.zip s3://stewartw-test-bucket/TimelineLite.TimelineEvent.zip
-aws s3 cp .\TimelineLite.TimelineEvent.zip s3://stewartw-test-bucket/TimelineLite.TimelineEventAttachment.zip
+aws s3 cp .\TimelineLite.TimelineEventAttachment.zip s3://stewartw-test-bucket/TimelineLite.TimelineEventAttachment.zip
