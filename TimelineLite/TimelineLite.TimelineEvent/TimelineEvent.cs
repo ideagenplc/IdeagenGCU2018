bcdf19372e441f0bfb49d54230e9d06e3f891f41
Ross Anderson <ross.anderson@ideagenplc.com>
2018-02-12 11:50:06 +0000

    adding serialization to lambda's

--- TimelineLite/TimelineLite.TimelineEvent/TimelineEvent.cs
+++ TimelineLite/TimelineLite.TimelineEvent/TimelineEvent.cs
@@ -12,6 +12,9 @@ using TimelineLite.StorageRepos;
 using static TimelineLite.Requests.RequestHelper;
 using static TimelineLite.Responses.ResponseHelper;
 
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
 namespace TimelineLite
 {
     public class TimelineEvent : LambdaBase
--- TimelineLite/TimelineLite.TimelineEventAttchment/TimelineEventAttachment.cs
+++ TimelineLite/TimelineLite.TimelineEventAttchment/TimelineEventAttachment.cs
@@ -10,6 +10,9 @@ using Newtonsoft.Json;
 using TimelineLite.Requests;
 using TimelineLite.Responses;
 
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
 namespace TimelineLite.TimelineEventAttachment
 {
     public class TimelineEventAttachment : LambdaBase
