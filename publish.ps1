9d8499ba395e0cbaff2bb7b0e08399092b61f786
Ross Anderson <ross.anderson@ideagenplc.com>
2018-02-13 14:30:54 +0000

    Fixing assembly name

--- publish.ps1
+++ publish.ps1
@@ -2,8 +2,8 @@ dotnet publish .\TimelineLite\  -c Release  -f netcoreapp2.0 /p:GenerateRunTimeC
 
 Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.zip -Force
 Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.Timeline\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.Timeline.zip -Force
-Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.TimelineEvent\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.TimeLineEvent.zip -Force
-Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.TimelineEventAttchment\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.TimeLineEventAttachment.zip -Force
+Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.TimelineEvent\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.TimelineEvent.zip -Force
+Compress-Archive -Path C:\Work\IdeagenGCU2018\TimelineLite\TimelineLite.TimelineEventAttachment\bin\Release\netcoreapp2.0\publish\* -DestinationPath TimelineLite.TimelineEventAttachment.zip -Force
 
 aws s3 cp .\TimelineLite.zip s3://stewartw-test-bucket/TimelineLite.zip
 aws s3 cp .\TimelineLite.Timeline.zip s3://stewartw-test-bucket/TimelineLite.Timeline.zip
